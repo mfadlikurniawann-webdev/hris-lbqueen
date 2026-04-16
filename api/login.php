5249a952db8ed2675c3cd5bed17a15b791556a4f
M Fadli Kurniawan <mfadlikurniawann@gmail.com>
2026-04-16 16:45:12 +0700

    Update: icon web tab

--- api/login.php
+++ api/login.php
@@ -18,7 +18,7 @@ if ($payload) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-    
+    <link rel="icon" type="image/png" href="/logo/lbqueen_logo.PNG">
     <style>
         :root {
             --lbq-pink: #b6416a; /* Warna pink persis seperti di gambar */
