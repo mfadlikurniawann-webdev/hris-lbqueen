bb9e438e5e7fd7ad0b428027a4199ef51859161c
M Fadli Kurniawan <mfadlikurniawann@gmail.com>
2026-04-21 14:55:37 +0700

    update

--- api/index.php
+++ api/index.php
@@ -237,7 +237,7 @@ if ($is_admin) {
     <link rel="apple-touch-icon" href="/logo/lbqueen_logo.PNG">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
-    <link rel="stylesheet" href="/style/style.css">
+    <link rel="stylesheet" href="/style/style.css?v=<?= time() ?>">
 </head>
 
 <body>
